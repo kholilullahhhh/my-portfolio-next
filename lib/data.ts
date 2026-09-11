28e9d5b4d190588b2c94753bf2b15a5bfd3b698e
kholilullahhhh <muhammadkholil045@gmail.com>
2026-09-11 18:16:49 +0800

    chore: update data libs

--- lib/data.ts
+++ lib/data.ts
@@ -45,7 +45,7 @@ export const projects: Project[] = [
     id: "4",
     title: "Otomatisasi Absensi Siswa dengan Pencocokan Wajah",
     description:
-      "Solusi presensi pintar untuk SMP Aisyiyah Paccinongang. Memangkas waktu pencatatan manual, meningkatkan akurasi kehadiran, dan terintegrasi secara real-time.",
+      "Solusi presensi untuk SMP . Memangkas waktu pencatatan manual, meningkatkan akurasi kehadiran, dan secara real-time.",
     longDescription:
       "Sistem absensi siswa berbasis face recognition yang dirancang untuk mengotomatisasi proses pencatatan kehadiran di SMP Aisyiyah Paccinongang. Menggunakan YOLOv8 untuk deteksi wajah dan FaceNet untuk menghasilkan serta mencocokkan embedding wajah siswa. Sistem mendukung proses pengenalan wajah secara real-time dan menyimpan hasil absensi secara aman ke dalam database.",
     image: "/image.png",
